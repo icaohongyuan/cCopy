4f144b4ed7d1ee0912bd7b9dd33d5c684f81b418
caohongyuan <895467839@qq.com>
2020-01-08 17:28:36 +0800

    v1.0.1

--- ccopy.js
+++ ccopy.js
@@ -1,5 +1,5 @@
 /*!
- * ccopy.js v1.0.0 (2019-12-20)
+ * ccopy.js v1.0.1 (2020-01-08)
  *
  * @link https://plugin.caohongyuan.cn/copy
  * @auther https://caohongyuan.cn/
@@ -7,42 +7,58 @@
 (function (window, document) {
     "use strict";
     var copyContent = '';
+    var contentType;
+    var copyDom;
+    var button;
+
     function template() {
-    	var button = document.createElement("input");
-        button.setAttribute("type", "button");
+        button = document.createElement("span");
+        button.setAttribute("id", "copyButton");
         button.setAttribute("onclick", "cCopy('ccopy')");
-		button.setAttribute("value", "复制");
-		button.setAttribute("style", 'cursor: pointer; margin: 10px;');
-		return button;
+        button.setAttribute("style", 'cursor: pointer; margin: 10px; display: none; border: 1px solid; border-radius: 4px;');
+        button.innerText = '复制';
+        return button;
     }
 
     function createHtml() {
-    	var copyDom = document.getElementById('ccopy');
-    	try {
-            var contentType = copyDom.dataset.ccopy;
+        copyDom = document.getElementById('ccopy');
+
+        try {
+            contentType = copyDom.dataset.ccopy;
         } catch (err) {
-            var contentType = '';
+            contentType = '';
         }
         if (contentType === undefined || contentType === '' || contentType === 'text') {
             copyContent = copyDom.innerText;
         }
         if (contentType === 'html') {
-        	copyContent = copyDom.innerHTML;
+            copyContent = copyDom.innerHTML;
         }
         copyDom.appendChild(template());
     }
 
     function copy(content) {
-        var aux = document.createElement("input"); 
-        aux.setAttribute("value", content); 
-        document.body.appendChild(aux); 
+        var aux = document.createElement("input");
+        aux.setAttribute("value", content);
+        document.body.appendChild(aux);
         aux.select();
-        document.execCommand("copy"); 
+        document.execCommand("copy");
         document.body.removeChild(aux);
-        alert('已复制');
+        button.innerText = '已复制';
+        setTimeout(function(){button.innerText = '复制';}, 1000);
     }
+
+
     createHtml();
     window.cCopy = function () {
-    	copy(copyContent);
-    }
+        copy(copyContent);
+    };
+
+    var copyButton = document.getElementById('copyButton');
+    copyDom.onmouseover = function () {
+        copyButton.style.display = '';
+    };
+    copyDom.onmouseout = function () {
+        copyButton.style.display = 'none';
+    };
 })(window, document);
